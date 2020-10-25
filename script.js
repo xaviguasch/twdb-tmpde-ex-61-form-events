c997f07692b46fb6398d1e715ad05d35344b1436
xaviguasch <xaviguasch@gmail.com>
2020-10-25 15:44:57 +0100

    completed exercise

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log('hey')
+const form = document.querySelector('form')
+const ul = document.querySelector('#list')
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  let productInput = form.elements.product
+  let qtyInput = form.elements.qty
+
+  addNewItem(productInput.value, qtyInput.value)
+
+  productInput.value = ''
+  qtyInput.value = ''
+})
+
+function addNewItem(product, qty) {
+  const li = document.createElement('li')
+  li.innerText = `${product} - ${qty}`
+
+  ul.append(li)
+}
